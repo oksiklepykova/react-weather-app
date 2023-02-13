9d468dbb55140d537f6b44f792aee4ed793a6436
Пользователь <oksiklepykova@gmail.com>
2023-02-13 18:45:39 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1> 
+      <footer>
+        This project is created by <a href="/" target="_blank" rel="noreferrer">Oksana Klepykova</a> and is{" "}
+      <a href="https://github.com/oksiklepykova/react-weather-app" target="_blank" rel="noreferrer">
+        open-sourced on GitHub 
+      </a>
+      </footer>
     </div>
   );
 }
