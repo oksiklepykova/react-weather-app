96e757105a255563d54000ea5b1741f7527b7641
Пользователь <oksiklepykova@gmail.com>
2023-02-24 15:53:33 +0300

    Added structure in the WeatherForecastDay file

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactAnimatedWeather from 'react-animated-weather';
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
     const codeMapping = {
@@ -7,7 +7,7 @@ export default function WeatherIcon(props) {
         "01n": "CLEAR_NIGHT",
         "02d": "PARTLY_CLOUDY_DAY",
         "02n": "PARTLY_CLOUDY_NIGHT",
-        "03d": "PARTLY_CLOUDY_DAYY",
+        "03d": "PARTLY_CLOUDY_DAY",
         "03n": "PARTLY_CLOUDY_NIGHT",
         "04d": "CLOUDY",
         "04n": "CLOUDY",
