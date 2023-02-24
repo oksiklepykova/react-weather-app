7f0218c2da9605964dd5f2d3a9aecd2116a17cca
Пользователь <oksiklepykova@gmail.com>
2023-02-24 10:26:00 +0300

    Added coordinates  in the project

--- src/Weather.js
+++ src/Weather.js
@@ -9,10 +9,12 @@ import './Weather.css';
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
     const [city, setCity] = useState(props.defaultCity);
+
     function handleResponse(response) {
  //    console.log(response.data);
       setWeatherData ({
         ready: true,
+        coordinates: response.data.coord,
         temperature: response.data.main.temp,
         humidity: response.data.main.humidity,
         date: new Date(response.data.dt * 1000),
@@ -57,7 +59,7 @@ export default function Weather(props) {
                </div> 
             </form>
             <WeatherInfo data={weatherData} />
-            <WeatherForecast />
+            <WeatherForecast coordinates={weatherData.coordinates} />
         </div>
     ); 
  } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import WeatherIcon from './WeatherIcon';
 import './WeatherForecast.css';
+import axios from 'axios';
 
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+    function handleResponse(response) {
+        console.log(response.data)
+    }
+
+    let apiKey = 'cb286bad3607984b41ed10c8de5cf00e';
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
    return (
         <div className='WeatherForecast'>
             <div className='row'>
@@ -17,5 +27,5 @@ export default function WeatherForecast() {
                </div>
             </div>
         </div>
-   )
+   ) 
 }
